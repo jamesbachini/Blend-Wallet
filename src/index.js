21b74d7ffacc21e08be2c624c46ff17c9f2a45cb
Anon <anon@tmp.com>
2025-04-09 19:03:15 +0200

    balances working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { PoolContract, RequestType } from '@blend-capital/blend-sdk';
-import { Keypair, TransactionBuilder, Networks, BASE_FEE, rpc, xdr, nativeToScVal, Contract } from 'stellar-sdk';
+import { Keypair, TransactionBuilder, Networks, BASE_FEE, rpc, xdr, nativeToScVal, scValToNative, Contract } from 'stellar-sdk';
 
 // Attach to window so it’s available globally
 window.BlendSdk = { PoolContract, RequestType };
-window.StellarSdk = { Keypair, TransactionBuilder, Networks, BASE_FEE, rpc, xdr, nativeToScVal, Contract };
+window.StellarSdk = { Keypair, TransactionBuilder, Networks, BASE_FEE, rpc, xdr, nativeToScVal, scValToNative, Contract };
